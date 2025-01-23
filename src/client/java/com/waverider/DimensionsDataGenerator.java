0dc0486787b22876a1c35763357ddc3287e3aee5
Wave_Rider_ <camerontowver@gmail.com>
2025-01-23 15:07:20 -0700

    init commit again

---
+++ src/client/java/com/waverider/DimensionsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.waverider;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DimensionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
